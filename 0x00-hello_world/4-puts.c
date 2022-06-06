8a30e23a90fff667589c040bc2bb30b085c60a04
Hostepnix <stephane4love@gmail.com>
2022-06-06 15:53:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - program entry point
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-			}
+}
